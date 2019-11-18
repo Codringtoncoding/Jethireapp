6a4a95758d3b0c685445c1499aa012ad33ce384d
Brigitta Szikszai <sziszka0001@gmail.com>
2019-11-18 17:38:28 +0000

    add the new and the create function

--- app/controllers/planes_controller.rb
+++ app/controllers/planes_controller.rb
@@ -3,11 +3,24 @@ class PlanesController < ApplicationController
   end
 
   def new
+    @plane = Plane.new
   end
 
   def create
+      @plane = Plane.new(plane_params)
+
+    if @plane.save
+      redirect_to plane_path(@plane)
+    else
+      render :new
+    end
   end
 
   def show
   end
+
+ def plane_params
+    params.require(:plane).permit(:name)
+  end
+
 end

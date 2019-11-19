b1138cab46056bf83b1c2e42bf339fb77aeab684
cyril najjar <najjarcyril@gmail.com>
2019-11-19 11:12:09 +0000

    Added users and planes in the seed. Created accounts for everyone as well. You can now login as your_first_name@lewagon.com, password is 123456 for all of our accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Plane.destroy_all
 
-Plane.create!(name: "Bombardier Global Express", user: User.first)
-Plane.create!(name: "Embraer Phenom 100", user: User.first)
-Plane.create!(name: "Gulfstream G550", user: User.first)
+humphrey = User.create(email: "humphrey@lewagon.com", password: "123456")
+cyril = User.create(email: "cyril@lewagon.com", password: "123456")
+brigita = User.create(email: "brigita@lewagon.com", password: "123456")
+luke = User.create(email: "luke@lewagon.com", password: "123456")
+
+
+Plane.create!(name: "Bombardier Global Express", user: humphrey, price: 2500, capacity: 15)
+Plane.create!(name: "Embraer Phenom 100", user: cyril, price: 2000, capacity: 12)
+Plane.create!(name: "Gulfstream G550", user: brigita, price: 1700, capacity: 10)
+Plane.create!(name: "LukeJet 3800 Sport Edition", user: luke, price: 1500, capacity: 8)
 
 

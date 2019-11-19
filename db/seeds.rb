15eeaa3a3cc75896e09546d6c59a98d060a6789b
Humphrey Codrington <Humphrey.codrington@gmail.com>
2019-11-19 14:57:48 +0000

    upload on plane cards. 14.38.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Plane.destroy_all
+
 Plane.create!(name: "Bombardier Global Express", user: User.first, price: 100000, capacity: 8)
 Plane.create!(name: "Embraer Phenom 100", user: User.first, price: 50000, capacity: 6)
 Plane.create!(name: "Gulfstream G550", user: User.first, price: 1000000, capacity: 30)

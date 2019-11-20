7a5df19f08670db0218f60b121086bfdf7883d5b
cyril najjar <najjarcyril@gmail.com>
2019-11-20 14:12:15 +0000

    Fixed the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
-Plane.destroy_all
 Booking.destroy_all
+Plane.destroy_all
+User.destroy_all
 
 
 humphrey = User.create(email: "humphrey@lewagon.com", password: "123456")

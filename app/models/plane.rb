465efbc73933217cc681117e6557e7d4b58fbb0f
luke smyth-osbourne <lukesmythosbourne@gmail.com>
2019-11-21 14:16:46 +0000

    working on reviews

--- app/models/plane.rb
+++ app/models/plane.rb
@@ -2,5 +2,4 @@ class Plane < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
-  validates :photo, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_120432) do
+ActiveRecord::Schema.define(version: 2019_11_21_112407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,15 @@ ActiveRecord::Schema.define(version: 2019_11_20_120432) do
     t.index ["user_id"], name: "index_planes_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "planes_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["planes_id"], name: "index_reviews_on_planes_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -73,4 +82,5 @@ ActiveRecord::Schema.define(version: 2019_11_20_120432) do
   add_foreign_key "bookings", "planes"
   add_foreign_key "bookings", "users"
   add_foreign_key "planes", "users"
+  add_foreign_key "reviews", "planes", column: "planes_id"
 end

1d93446d4c8102f7e098e66b690ec795f00bd99a
Brigitta Szikszai <sziszka0001@gmail.com>
2019-11-21 16:49:18 +0000

    fixed the calendars

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -2,7 +2,9 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css"; // Note this is important!
 
 flatpickr(".datepicker", {
-  altInput: true
+ // altInput: true
+  minDate: "today",
+  maxDate: new Date().fp_incr(180) // 180 days from now
 })
 
 flatpickr(".timepicker", {

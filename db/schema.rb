1bf5bea1ab6f405da33508b2201f3bdd08aece0c
Humphrey Codrington <Humphrey.codrington@gmail.com>
2019-11-22 15:40:31 +0000

    big fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_11_22_113458) do
-=======
-ActiveRecord::Schema.define(version: 2019_11_21_164329) do
->>>>>>> f3a9be2d0f48c215ede1801edf8d670b4893a1fb
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
